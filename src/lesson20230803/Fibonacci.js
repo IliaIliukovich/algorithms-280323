476d292d392088c6900c3231f615bb7cd23cd33c
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2023-08-10 12:20:13 +0200

    Recursion optimisations

--- src/lesson20230803/Fibonacci.js
+++ src/lesson20230803/Fibonacci.js
@@ -22,5 +22,35 @@ function fibonacciMemoRecursion(n, cache) {
     return cache[n];
 }
 
-console.log(fibonacci(10));
-console.log(fibonacciMemo(10));
+function fibonacciTabulation(n){
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+    let cache = new Array(n + 1);
+    cache.fill(0);
+    cache[1] = 1;
+
+    for (i = 2; i < n + 1; i++){
+        cache[i] = cache[i - 1] + cache[i - 2];
+    }
+    return cache[n];
+}
+
+function fibonacciTabulationOpimized(n){
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+    let cache1 = 0;
+    let cache2 = 1;
+    let result = 1;
+
+    for (i = 2; i < n + 1; i++){
+        result = cache1 + cache2;
+        cache1 = cache2;
+        cache2 = result;
+    }
+    return result;
+}
+
+console.log(fibonacciMemo(45));
+console.log(fibonacciTabulation(45));
+console.log(fibonacciTabulationOpimized(45));
+console.log(fibonacci(45));
